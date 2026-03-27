c64b2ec637063722dd0dc6ed1ae461f4fcb05343
StachinskiiVS <boss20276@gmail.com>
2026-03-27 15:35:04 +0500

    Task4

---
+++ Tyuiu.StachinskiiVS.Sprint4.Task4.V30.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.StachinskiiVS.Sprint4.Task4.V30.Lib
+{
+    public class DataService : ISprint4Task4V30
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+            int[,] array = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        array[i, j] = 0;
+                    }
+                    else
+                    {
+                        array[i, j] = matrix[i, j];
+                    }
+                }
+            }
+            return array;
+        }
+
+    }
+}
---
+++ Tyuiu.StachinskiiVS.Sprint4.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.StachinskiiVS.Sprint4.Task4.V30.Lib;
+
+namespace Tyuiu.StachinskiiVS.Sprint4.Task4.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5] { { 3, 4, 6, 6, 5 },
+                                         { 6, 7, 6, 3, 5 },
+                                         { 5, 3, 5, 7, 6 },
+                                         { 6, 4, 7, 6, 6 },
+                                         { 7, 4, 3, 4, 5 } };
+            int[,] res = ds.Calculate(matrix);
+            int[,] wait = { { 0, 4, 6, 6, 0 },
+                            { 6, 0, 6, 0, 0 },
+                            { 0, 0, 0, 0, 6 },
+                            { 6, 4, 0, 6, 6 },
+                            { 0, 4, 0, 4, 0 } }; ;
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
