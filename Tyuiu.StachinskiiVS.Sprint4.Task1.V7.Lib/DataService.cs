9fda49089287c6338c88567e311f9bb64220a4ad
StachinskiiVS <boss20276@gmail.com>
2026-03-27 15:25:07 +0500

    Task1

---
+++ Tyuiu.StachinskiiVS.Sprint4.Task1.V7.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.StachinskiiVS.Sprint4.Task1.V7.Lib
+{
+    public class DataService : ISprint4Task1V7
+    {
+        public int Calculate(int[] array)
+        {
+            int s = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                    s += array[i];
+            }
+            return s;
+        }
+    }
+}
