906a2c2c2752b4b865da3278a54f0d3779eb539d
StachinskiiVS <boss20276@gmail.com>
2026-03-27 15:20:49 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.StachinskiiVS.Sprint4.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.StachinskiiVS.Sprint4.Task0.V19.Lib;
+
+namespace Tyuiu.StachinskiiVS.Sprint4.Task0.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultOddArrEl()
+        {
+            DataService ds = new DataService();
+            int[] array = { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 };
+            int res = ds.GetMultOddArrEl(array);
+            int wait = 297675;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
